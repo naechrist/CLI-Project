e1b1b00eb2292f97d718ac6221a507a78e948997
Jenae Christensen <729jenae.christensen@gmail.com>
2020-09-23 13:26:49 -0400

    Update cli.rb

--- lib/louisville_events/cli.rb
+++ lib/louisville_events/cli.rb
@@ -3,6 +3,7 @@ class LouisvilleEvents::CLI
     def call 
         Scraper.scrape_events 
         puts "Welcome to Kentuckiana (Louisville, Kentucky and Southern Indiana)! Check out the list below to see the upcoming events."
+        puts ""
         menu
     end
 
@@ -11,14 +12,25 @@ class LouisvilleEvents::CLI
         events.each.with_index(1) do |event, index|
             puts "#{index}. #{event.name} on #{event.month} #{event.day}, #{event.year}"
         end
+        puts ""
         puts "Enter the number of the event you'd like more info on."
-        input = gets.strip
-        index = input.to_i - 1
-        if event = Event.all[index]
-        puts "#{event.name}: #{event.summary}"
-        puts "Hope you found something good! If not, type list to see the list again or exit to exit the program."
-        input = gets.strip 
-            if input.downcase == "list"
+        input = gets.strip          #takes in user input
+        index = input.to_i - 1      #computer starts reading @ 0 
+            if event = Event.all[index]     # if number by event name is entered...
+            puts ""
+            puts "#{event.name}: #{event.summary}"
+            puts ""
+            puts "Hope you found something good! If not, type list to see the list again or exit to exit the program."
+            input = gets.strip 
+                if input.downcase == "list"
+                    list
+                elsif input.downcase == "exit"
+                    exit
+                else 
+                    invalid
+                    menu 
+                end
+            elsif input.downcase == "list"
                 list
             elsif input.downcase == "exit"
                 exit
@@ -26,15 +38,6 @@ class LouisvilleEvents::CLI
                 invalid
                 menu 
             end
-        elsif input.downcase == "list"
-            list
-        elsif input.downcase == "exit"
-            exit
-        else 
-            invalid
-            menu 
-        end
-
     end
 
     def list 
@@ -42,10 +45,12 @@ class LouisvilleEvents::CLI
     end
 
     def exit 
+         puts ""
          puts "Thanks for stopping by, hope to see you soon!!"
     end
 
     def invalid 
-        puts "Sorry, I don't understand."
+        puts "Sorry, I don't understand, please enter the number of the event you'd like more info on."
+        
     end
 end
